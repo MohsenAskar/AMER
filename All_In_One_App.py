412e6a6c436c82477a5dd7afc859e9d11eaaaeee
MohsenAskar <71376243+MohsenAskar@users.noreply.github.com>
2024-09-24 17:28:23 +0200

    Update All_In_One_App.py

--- All_In_One_App.py
+++ All_In_One_App.py
@@ -33,8 +33,8 @@ st.markdown(
         padding: 10px;
         flex-direction: column; /* Stack items vertically */
         position: absolute;
-        top: 15%;  /* Adjust vertical position to be slightly higher */
-        right: -18%; /* Horizontal center */
+        top: 10%;  /* Adjust vertical position to be slightly higher */
+        right: -10%; /* Horizontal center */
         transform: translate(-50%, -40%); /* Adjust to keep the element centered with the new top value */
     }}
     .header img {{
