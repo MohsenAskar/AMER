eeb581a9813414116aaec7ce750694e7e403786d
MohsenAskar <71376243+MohsenAskar@users.noreply.github.com>
2024-09-25 10:31:29 +0200

    Update All_In_One_App.py

--- All_In_One_App.py
+++ All_In_One_App.py
@@ -30,7 +30,7 @@ st.markdown(
         display: flex;
         justify-content: center; /* Center horizontally */
         align-items: center; /* Center vertically */
-        padding: 12px;
+        padding: 14px;
         flex-direction: column; /* Stack items vertically */
         position: absolute;
         top: 0%;  /* Adjust vertical position to be slightly higher */
